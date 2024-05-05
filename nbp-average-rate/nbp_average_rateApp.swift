f113846e46b60cc865464df8f942c2708c499e34
Adam Golczak <adam.golczak@appunite.com>
2024-05-05 18:07:03 +0200

    Initial Commit

---
+++ nbp-average-rate/nbp_average_rateApp.swift
@@ -0,0 +1,17 @@
+//
+//  nbp_average_rateApp.swift
+//  nbp-average-rate
+//
+//  Created by Adam Golczak on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct nbp_average_rateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
